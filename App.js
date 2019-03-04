68f03c95d95e0a7b83af48699a1b6870c40800c9
Shota3F4 <shoutanakka1012@gmail.com>
2019-03-05 04:07:39 +0900

    firest contact:

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style = {styles.appbar}>
+          
+        </View>
+      </View > 
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
