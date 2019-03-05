f1d2edfb05f42568ef92627a750b21d3e6417034
Shota3F4 <shoutanakka1012@gmail.com>
2019-03-06 00:31:18 +0900

    抵抗コンポーネントの追加

---
+++ src/components/Resistanse.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {StyleSheet, View ,Text} from 'react-native'
+
+class Resistance extends React.Component{
+  render(){
+    return (
+      <View>
+      <Text>"Resistance"</Text>
+      </View>
+    );
+  }
+}
+
+
+export default Resistance
