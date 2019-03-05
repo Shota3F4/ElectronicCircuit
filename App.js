007940c1b65fac379b3a140c0f63d311bf12ce59
Shota3F4 <shoutanakka1012@gmail.com>
2019-03-06 00:29:03 +0900

    抵抗コンポーネントの読み込み

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Resistance from './assets/src/components/Resistanse'
+
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <View style = {styles.appbar}>
-          
+          <Resistance />
         </View>
       </View > 
     );
@@ -16,7 +18,7 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 2,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
