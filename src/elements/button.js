a4bf0014eef97da6ddfa39556957f2a1a1787bf1
Shota3F4 <shoutanakka1012@gmail.com>
2019-03-08 23:12:46 +0900

    Botton Element の追加(style,color,size,onPress...)をpropsに

---
+++ src/elements/button.js
@@ -0,0 +1,64 @@
+import React,{Component} from 'react'
+import {StyleSheet,View,Text ,TouchableHighlight} from 'react-native'
+import {Font} from 'expo'
+import Fontawesome from '../../assets/fa-solid-900.ttf'
+
+class Button extends Component{
+  state={
+    fontLoaded:false
+  }
+  async componentWillMount(){
+    await Font.loadAsync({
+      fontawesome: Fontawesome
+    });
+    this.setState({fontLoaded:true})
+  }
+  
+  render(){
+    const {url,color,style,onPress,size} = this.props;
+    
+
+    return (
+      <View style={[styles.container,style]}>
+       {this.state.fontLoaded ? (
+         <TouchableHighlight 
+         style = {[
+           styles.buttonstyle,
+           {
+             width:size,
+             height:size
+         }
+         ]}
+         underlayColor='transparent'
+         onPress ={onPress}
+         >
+          <Text style={[styles.buttontext,{color:color,fontSize:size}]}>{url}</Text>
+        </TouchableHighlight>
+       ):null
+       }
+      </View>
+    )
+  }
+}
+
+
+const styles=StyleSheet.create({
+  container:{
+    top:0,
+    left:0
+  },
+  buttonstyle:{
+    width:60,
+    height:60,
+  },
+  buttontext:{
+    justifyContent:'center',
+    alignSelf:'center',
+    fontFamily:'fontawesome',
+    fontSize:60,
+    color:'#000'
+  }
+
+})
+
+export default Button
