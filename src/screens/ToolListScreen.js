d244400df90b2aa62bf1d737a0866eabe1234c9d
Shota3F4 <shoutanakka1012@gmail.com>
2019-03-12 01:33:01 +0900

    素子リスト化部分のscreenを作成

---
+++ src/screens/ToolListScreen.js
@@ -0,0 +1,58 @@
+import React,{Component} from 'react'
+import {StyleSheet, View,Text,}from 'react-native'
+import ToolList from '../components/ToolList'
+import Button from '../elements/button'
+
+
+
+class ToolListScreen extends Component{
+  onPress(){
+    console.log('Hi')
+  }
+  render(){
+    return (
+      <View style = {styles.container}>
+        <View style = {styles.topstyle}>
+          <View style = {styles.returnbutton}>
+            <Button 
+              url = {'\uf101'} 
+              size = {105} 
+              color = '#fff'
+              style = {{top:0}}
+             onPress = {() => {this.onPress()}}
+              />
+          </View>
+        </View>
+        <View style = {styles.liststyle}>
+          <ToolList />
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:'#ffb'
+  },
+  topstyle:{
+    top:100,
+    flex:1,
+    alignSelf:'center',
+  },
+  returnbutton:{
+    backgroundColor:'#3ca2ff',
+    borderRadius:14,
+    alignItems:'center',
+   justifyContent: 'center',
+    width:220,
+    height:100
+  },
+  liststyle:{
+    flex:2,
+    paddingTop:10
+  },
+})
+
+export default ToolListScreen
