3e91929ced7794d447bc9c52a08019e285d59f42
Shota3F4 <shoutanakka1012@gmail.com>
2019-03-18 01:00:55 +0900

    最初にHomeScreen.jsが表示されるように

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import Resistance from './src/components/Resistanse'
 import ToolListScreen from './src/screens/ToolListScreen'
+import HomeScreen from './src/screens/HomeScreen'
 
 
 
@@ -10,7 +11,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style = {styles.container}> 
-          <ToolListScreen />
+          <HomeScreen />
           </View>
     );
   }
