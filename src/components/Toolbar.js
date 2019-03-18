5372db52316e85426486c2e8b0f89ac073f49f6a
Shota3F4 <shoutanakka1012@gmail.com>
2019-03-18 23:54:04 +0900

    必要なくなったfontAwesomeの処理を省いた

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,28 +1,12 @@
 import React from 'react';
 import {StyleSheet,View,Text,TouchableHighlight} from 'react-native';
-import {Font} from 'expo'
-import Fontawesome from '../../assets/fa-solid-900.ttf'
 import Button from '../elements/button'
 
 class Toolbar extends React.Component{
-  state={
-    fontLoaded:false
-  }
-  async componentWillMount(){
-    await Font.loadAsync({
-      fontawesome: Fontawesome
-    });
-    this.setState({fontLoaded:true})
-  }
-
-  PopDrawer(){
-    console.log("Pop!!");
-  }
     render(){
         return (
         <View style = {styles.container}>
         {
-          this.state.fontLoaded ? (
             <Button 
             style = { styles.buttonstyle} 
             color = '#162226' 
@@ -30,8 +14,6 @@ class Toolbar extends React.Component{
             size = {60}
             onPress = {() => {this.props.onPress()}}
             />
-          ):null
-          
         }
         </View>
         );
